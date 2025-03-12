a9309329a53217e627037d549f225fe6297713eb
cedie-22 <cedievirtud@gmail.com>
2025-03-12 22:50:57 +0800

    Add files via upload

---
+++ data/projects-data.js
@@ -0,0 +1,54 @@
+const projects = [
+  {
+    title: 'cedie-22.github.io',
+    image: 'ced.png', //Add your project-image in "images" folder
+    description: 'Project Description',
+    type: 'Website',
+    link: 'https://cedie-22.github.io/my-website/page.html'
+  },
+  {
+    title: 'cedie-22.github.io',
+    image: 'ced.png', //Add your project-image in "images" folder
+    description: 'Project Description',
+    type: 'website',
+    link: 'https://cedie-22.github.io/my-website/page.html'
+  },
+  {
+    title: 'blank',
+    image: 'ced.png', //Add your project-image in "images" folder
+    description: 'Project Description',
+    type: 'website',
+    link: 'https://cedie-22.github.io/my-website/page.html'
+  },
+  {
+    title: 'blank',
+    image: 'ced.png', //Add your project-image in "images" folder
+    description: 'Project Description',
+    type: 'website',
+    link: 'https://cedie-22.github.io/my-website/page.html'
+  },
+  {
+    title: 'blank',
+    image: 'ced.png', //Add your project-image in "images" folder
+    description: 'Project Description',
+    type: 'video',
+    link: 'https://cedie-22.github.io/my-website/page.html'  //if your project is video type, upload your video in any social media or gdrive and paste the link here
+  },
+  {
+    title: 'blank',
+    image: 'ced.png', //Add your project-image in "images" folder
+    description: 'Project Description',
+    type: 'video',
+    link: 'https://cedie-22.github.io/my-website/page.html' //if your project is video type, upload your video in any social media or gdrive and paste the link here
+  },
+  {
+    title: 'blank',
+    image: 'ced.png', //Add your project-image in "images" folder
+    description: 'Project Description',
+    type: 'ui-design',
+    link: ''
+  }
+];
+
+
+export default projects;
